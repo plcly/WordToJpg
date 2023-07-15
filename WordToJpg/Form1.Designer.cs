4a135b8424bc09a61a158796d4f91f42692d6097
plcly <shuipingplcly@gmail.com>
2023-07-15 23:16:34 +0800

    添加项目文件。

---
+++ WordToJpg/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace WordToJpg
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.AllowDrop = true;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(28, 28);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(735, 389);
+            this.panel1.TabIndex = 1;
+            this.panel1.DragEnter += new System.Windows.Forms.DragEventHandler(this.panel1_DragEnter);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("宋体", 25.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(145, 173);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(431, 43);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "拖动Word文件转换JPG";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ WordToJpg/Form1.cs
@@ -0,0 +1,43 @@
+﻿using Aspose.Words;
+using Aspose.Words.Saving;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WordToJpg
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void panel1_DragEnter(object sender, DragEventArgs e)
+        {
+            var files = e.Data.GetData(DataFormats.FileDrop) as string[];
+            if (files.Length > 0)
+            {
+                var extension = Path.GetExtension(files[0]);
+                if (extension == ".docx" || extension == ".doc" || extension == ".wps")
+                {
+                    var path = files[0];
+                    var jpgName = Path.Combine(Path.GetDirectoryName(path), Path.GetFileNameWithoutExtension(path)+".jpg");
+                    var doc = new Document(path);
+                    ImageSaveOptions options = new ImageSaveOptions(SaveFormat.Jpeg);
+                    options.Resolution = 300;
+                    options.JpegQuality = 100;
+                    options.UseHighQualityRendering = true;
+                    doc.Save(jpgName, options);
+                }
+            }
+        }
+    }
+}
